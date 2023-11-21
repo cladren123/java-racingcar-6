963c7d529cf19ebd1492d7e235e2114a69eeb3e3
KimDongEon <gold3987@naver.com>
2023-11-22 02:31:50 +0900

    feat(InputView) : 자동차 이름 입력 구현 - inputCar()

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,20 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import constant.InputMessage;
+
+/**
+ * 사용자로부터 경주 할 자동차의 이름과 시도할 회수를 입력 받습니다.
+ */
 public class InputView {
+
+  /**
+   * 자동차 이름을 입력 받습니다.
+   * @return : String
+   */
+  public static String inputCar() {
+    System.out.println(InputMessage.CAR_NAME.getMessage());
+    return Console.readLine().trim();
+  }
+
 }
