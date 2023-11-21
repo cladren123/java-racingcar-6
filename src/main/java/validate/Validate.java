cbd1aae2e68139ee303a275b39bb8cfb6e0ce0e4
KimDongEon <gold3987@naver.com>
2023-11-22 02:52:58 +0900

    feat(Validate) : 숫자 검증 메소드 구현 - validateNumber()

--- src/main/java/validate/Validate.java
+++ src/main/java/validate/Validate.java
@@ -1,4 +1,20 @@
 package validate;
 
+/**
+ * 검증 작업을 합니다.
+ */
 public class Validate {
+
+  /**
+   * 숫자인지 검증
+   */
+  public static void validateNumber(String input) {
+    try {
+      Integer.parseInt(input);
+    }
+    catch (NumberFormatException e) {
+      throw new IllegalArgumentException();
+    }
+  }
+
 }
