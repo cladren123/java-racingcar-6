a508f677bb6d8b5205e0a37240b0849ac23c9f35
KimDongEon <gold3987@naver.com>
2023-11-22 13:27:21 +0900

    feat(Race) : 현재 결과를 취합해 반환하는 메소드 구현 - round()

--- src/main/java/model/Race.java
+++ src/main/java/model/Race.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Race {
 
@@ -16,6 +18,21 @@ public class Race {
   public void move() {
     cars.forEach(Car::move);
   }
+
+  /**
+   * List의 Car의 현재 결과를 취합해서 반환합니다.
+   * @return Map<String,Integer>
+   */
+  public Map<String, Integer> round() {
+    Map<String, Integer> result = new LinkedHashMap<>();
+    for(Car car : cars) {
+      result.putAll(car.getRound());
+    }
+    return result;
+
+  }
+
+
 
 
 
