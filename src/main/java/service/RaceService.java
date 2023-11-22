e1439b58f5230dcd12b3620e086550ec8cf92815
KimDongEon <gold3987@naver.com>
2023-11-22 17:03:19 +0900

    feat(RaceService) : 입력 받은 자동차를 검증하고 저장 메소드 구현 - inputCar()

--- src/main/java/service/RaceService.java
+++ src/main/java/service/RaceService.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.Car;
+import model.Race;
+import util.Converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RaceService {
+
+  private Race race;
+
+
+  /**
+   * 자동차 입력 
+   * @param input
+   */
+  public void inputCar(String input) {
+    List<String> splitInput = Converter.inputToCars(input);
+    List<Car> cars = new ArrayList<>();
+    for(String data : splitInput) {
+       cars.add(new Car(data));
+    }
+    race = new Race(cars);
+  }
+
 }
