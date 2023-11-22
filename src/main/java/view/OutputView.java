fd0bb6737a5ec7fd958a3438a0fc17cb680f88b5
KimDongEon <gold3987@naver.com>
2023-11-22 18:39:27 +0900

    feat(OutputView) : 라운드 결과 출력 메소드 구현 - printRound()

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,22 @@
 package view;
 
+import constant.OutputMessage;
+
+import java.util.Map;
+
 public class OutputView {
+
+  /**
+   * 라운드 결과를 출력합니다.
+   * @param round
+   */
+  public static void printRound(Map<Integer, Map<String, String>> round) {
+    System.out.println(OutputMessage.RESULT.getMessage());
+    for(int roundNumber : round.keySet()) {
+      for(String name : round.get(roundNumber).keySet()) {
+        System.out.println(String.format(OutputMessage.ROUND_FORM.getMessage(), name, round.get(roundNumber).get(name)));
+      }
+    }
+  }
+
 }
