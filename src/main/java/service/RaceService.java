33368c372ba08adaf66717f6485d1dfa7a7b0f1e
KimDongEon <gold3987@naver.com>
2023-11-22 17:30:44 +0900

    feat(RaceService) : 시도할 횟수 검증 후 저장

--- src/main/java/service/RaceService.java
+++ src/main/java/service/RaceService.java
@@ -2,7 +2,9 @@ package service;
 
 import model.Car;
 import model.Race;
+import model.TryNumber;
 import util.Converter;
+import validate.Validate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +12,11 @@ import java.util.List;
 public class RaceService {
 
   private Race race;
+  private TryNumber tryNumber;
 
 
   /**
-   * 자동차 입력 
+   * 자동차 입력
    * @param input
    */
   public void inputCar(String input) {
@@ -23,6 +26,14 @@ public class RaceService {
        cars.add(new Car(data));
     }
     race = new Race(cars);
+  }
+
+  /**
+   * 시도할 횟수 입력
+   */
+  public void inputTryNumber(String input) {
+    Validate.validateNumber(input);
+    tryNumber = new TryNumber(Integer.parseInt(input));
   }
 
 }
