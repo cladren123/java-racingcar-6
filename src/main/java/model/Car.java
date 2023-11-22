6e67bfa8bf53e8097f5745a3689fd02e7283e4c8
KimDongEon <gold3987@naver.com>
2023-11-22 12:52:51 +0900

    feat(Car) : 자동차 전진 메소드 구현 - move()

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,14 +1,29 @@
 package model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import constant.NumberConstant;
+
 public class Car {
 
   private final String name; // 자동차 이름
-  private final Integer location; // 전진도
+  private Integer location; // 전진도
 
   public Car(String name, Integer location) {
     this.name = name;
     this.location = location;
   }
-  
+
+  /**
+   * 무작위 값을 발생시켜 4보다 크면 전진 합니다.
+   */
+  public void move() {
+    int number = Randoms.pickNumberInRange(0, 9);
+    if(number > NumberConstant.FORWARD_LIMIT.getNumber()) {
+      location += 1;
+    }
+  }
+
+
+
 
 }
