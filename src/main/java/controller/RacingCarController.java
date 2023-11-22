c5067a0089704889f971bbb287cdaaba009eb625
KimDongEon <gold3987@naver.com>
2023-11-22 18:30:15 +0900

    feat(RacingCarController) : 기본 메소드 구현

--- src/main/java/controller/RacingCarController.java
+++ src/main/java/controller/RacingCarController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import service.RaceService;
+import view.InputView;
+
+/**
+ * 사용자의 입력값으로 서비스를 실행하고 결과값을 반환합니다.
+ */
+
 public class RacingCarController {
+
+  private final RaceService raceService = new RaceService();
+
+  /**
+   * 경주 게임을 시작합니다.
+   */
+  public void startRacing() {
+
+
+  }
+
+
+  /**
+   * 자동차 이름 입력
+   */
+  public void inputCar() {
+    String input = InputView.inputCar();
+
+
+  }
+
+  /**
+   * 시도할 횟수를 입력 받고 검증된 결과값을 받습니다.
+   */
+  public void inputTryNumber() {
+
+  }
+
+  /**
+   * 레이싱 게임을 진행한 후 결과값을 반환 받아 OutputView로 전달합니다.
+   */
+  public void racing() {
+
+  }
+
+  /**
+   * 최종 우승자 값을 반환 받아 OutputView로 전달합니다.
+   */
+  public void winner() {
+
+  }
+
+
+
+
 }
