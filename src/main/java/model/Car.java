2b97999a208ad2f05e8a3ce8bd20752d0fc499af
KimDongEon <gold3987@naver.com>
2023-11-22 12:37:51 +0900

    feat(Car) : Car 클래스의 변수, 생성자 구현

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Car {
+
+  private final String name; // 자동차 이름
+  private final Integer location; // 전진도
+
+  public Car(String name, Integer location) {
+    this.name = name;
+    this.location = location;
+  }
+  
+
 }
